9163e8977795ac8124da825cf1cec4ef028d5f16
luperez <luperez@student.42.fr>
2015-02-28 00:37:36 +0100

    Add initialization of ncurses

--- main.c
+++ main.c
@@ -6,17 +6,26 @@
 /*   By: luperez <luperez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 23:57:31 by luperez           #+#    #+#             */
-/*   Updated: 2015/02/28 00:32:40 by luperez          ###   ########.fr       */
+/*   Updated: 2015/02/28 00:37:06 by luperez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <curses.h>
 #include "game_2048.h"
 
+void	game(void)
+{
+
+}
+
 int		main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
 	initscr();
+	noecho();
+	keypad(stdscr, TRUE);
+	game();
+	endwin();
 	return (0);
 }
