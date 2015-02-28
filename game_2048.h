99db90d9cd21237c1b2895e712b9ed35859b707f
luperez <luperez@student.42.fr>
2015-02-28 05:00:47 +0100

    add loops

--- game_2048.h
+++ game_2048.h
@@ -6,15 +6,25 @@
 /*   By: luperez <luperez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/28 00:22:43 by luperez           #+#    #+#             */
-/*   Updated: 2015/02/28 00:32:25 by luperez          ###   ########.fr       */
+/*   Updated: 2015/02/28 04:54:05 by luperez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAME_2048_H
 # define GAME_2048_h
 
-#include <curses.h>
+#include <ncurses.h>
 
 #include "libft.h"
+
+typedef struct		s_game
+{
+	int		score_size;
+	int		x;
+	int		y;
+	WINDOW	*field;
+	WINDOW	*score;
+	WINDOW	*home;
+}					t_game;
 
 #endif
--- main.c
+++ main.c
@@ -6,26 +6,63 @@
 /*   By: luperez <luperez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 23:57:31 by luperez           #+#    #+#             */
-/*   Updated: 2015/02/28 00:37:06 by luperez          ###   ########.fr       */
+/*   Updated: 2015/02/28 05:00:10 by luperez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <curses.h>
 #include "game_2048.h"
+
+void	game_io(t_game *g)
+{
+	char	c;
+
+	while ((c = wgetch(g->home)) != ERR)
+	{
+		waddch(g->field, c);
+		wrefresh(g->field);
+		wrefresh(g->score);
+	}
+}
+
+void	game_windows(t_game *g)
+{
+	g->score_size = 10;
+	getmaxyx(stdscr, g->y, g->x);
+	g->home = newwin(g->y, g->x, 0, 0);
+	g->field = newwin(g->y - g->score_size, g->x, 0, 0);
+	g->score = newwin(g->score_size, g->x, g->y - g->score_size, 0);
+}
+
+void	background(t_game *g)
+{
+	wborder(g->field, '|', '|', '-', '-', '+', '+', '+', '+');
+	mvwprintw(g->field, 0, 0, "Field");
+	mvwprintw(g->score, 0, 0, "Score");
+	wborder(g->score, '|', '|', '-', '-', '+', '+', '+', '+');
+	wrefresh(g->home);
+	wrefresh(g->field);
+	wrefresh(g->score);
+}
 
 void	game(void)
 {
+	t_game	game;
 
+	ft_bzero(&game, sizeof(t_game));
+	game_windows(&game);
+	background(&game);
+	sleep(100);
+	delwin(game.field);
+	delwin(game.score);
 }
 
-int		main(int argc, char **argv)
+int main(void)
 {
-	(void)argc;
-	(void)argv;
+	curs_set(FALSE);
 	initscr();
 	noecho();
-	keypad(stdscr, TRUE);
+	cbreak();
 	game();
 	endwin();
-	return (0);
+	return 0;
 }
