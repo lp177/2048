f6442c0c18e658229a243a66e339a8c2dae56e5f
luperez <luperez@student.42.fr>
2015-02-28 06:30:23 +0100

    Resize powwaa

--- main.c
+++ main.c
@@ -33,12 +33,54 @@ void	game_windows(t_game *g)
 	g->score = newwin(g->score_size, g->x, g->y - g->score_size, 0);
 }
 
+void	borders(WINDOW *screen)
+{
+	int x, y, i;
+	getmaxyx(screen, y, x);
+	// 4 corners
+	mvwprintw(screen, 0, 0, "+");
+	mvwprintw(screen, y - 1, 0, "+");
+	mvwprintw(screen, 0, x - 1, "+");
+	mvwprintw(screen, y - 1, x - 1, "+");
+	// sides
+	for (i = 1; i < (y - 1); i++) {
+		mvwprintw(screen, i, 0, "|");
+		mvwprintw(screen, i, x - 1, "|");
+	}
+	// top and bottom
+	for (i = 1; i < (x - 1); i++) {
+		mvwprintw(screen, 0, i, "-");
+		mvwprintw(screen, y - 1, i, "-");
+	}
+}
+
 void	background(t_game *g)
 {
-	wborder(g->field, '|', '|', '-', '-', '+', '+', '+', '+');
-	mvwprintw(g->field, 0, 0, "Field");
-	mvwprintw(g->score, 0, 0, "Score");
-	wborder(g->score, '|', '|', '-', '-', '+', '+', '+', '+');
+	borders(g->field);
+	borders(g->score);
+	mvwprintw(g->field, 1, 1, "Field");
+	mvwprintw(g->score, 1, 1, "Score");
+}
+
+void	resize(t_game *g)
+{
+	int	new_y;
+	int	new_x;
+
+	getmaxyx(stdscr, new_y, new_x);
+	if (new_y != g->y || new_x != g->x)
+	{
+		g->x = new_x;
+		g->y = new_y;
+		wresize(g->field, new_y - g->score_size, new_x);
+		wresize(g->score, g->score_size, new_x);
+		mvwin(g->score, new_y - g->score_size, 0);
+		wclear(stdscr);
+		wclear(g->home);
+		wclear(g->field);
+		wclear(g->score);
+		background(g);
+	}
 	wrefresh(g->home);
 	wrefresh(g->field);
 	wrefresh(g->score);
@@ -51,18 +93,19 @@ void	game(void)
 	ft_bzero(&game, sizeof(t_game));
 	game_windows(&game);
 	background(&game);
-	sleep(100);
+	while(1)
+		resize(&game);
 	delwin(game.field);
 	delwin(game.score);
 }
 
 int main(void)
 {
-	curs_set(FALSE);
 	initscr();
 	noecho();
 	cbreak();
+	curs_set(0);
 	game();
-	endwin();
+	//endwin();
 	return 0;
 }
